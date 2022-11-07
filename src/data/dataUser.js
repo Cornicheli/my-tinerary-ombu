572597db9cc2fbff46d0e89974740d066dc5e637
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-07 15:07:15 -0300

    mt-f1-01

---
+++ src/data/dataUser.js
@@ -0,0 +1,42 @@
+const used =[
+    {
+        id: 'user1',
+        name: 'Ignacio',
+        lastName: 'Borraz',
+        age: 25,
+        password: 'borraz1234',
+        code: '1a2b3c',
+        verifed: 'true',
+        logged: 'true'
+    },
+    {
+    id: 'user2',
+        name: 'Jose',
+        lastName: 'Lopez',
+        age: 30,
+        password: 'lopez1234',
+        code: '4d5c6e',
+        verifed: 'true',
+        logged: 'true'
+    },
+    {
+        id: 'user3',
+        name: 'Jose',
+        lastName: 'Lopez',
+        age: 30,
+        password: 'lopez1234',
+        code: '5f6h7i',
+        verifed: 'true',
+        logged: 'true'
+    },
+    {
+        id: 'user4',
+        name: 'Jose',
+        lastName: 'Lopez',
+        age: 30,
+        password: 'lopez1234',
+        code: 'j8k9l10',
+        verifed: 'true',
+        logged: 'true'
+    },
+]
